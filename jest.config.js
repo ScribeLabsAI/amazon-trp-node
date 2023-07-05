a68bc2160e690bb519913aaefbe4fb0e811b92fc
Ailin Venerus <ailinvenerus@gmail.com>
2023-07-05 18:10:35 +0100

    Bin and test added (#21)
    * Adding bin to check created document and adding test to check rows of doc created correctly
    
    * Putting back line in jest.config

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,13 @@ export default {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   testRegex: 'tests/.*\\.test\\.ts',
+  testTimeout: 10_000,
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
